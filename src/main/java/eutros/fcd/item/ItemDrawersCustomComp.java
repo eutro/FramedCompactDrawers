f40c5837d9a258854fc3f6b24fc17f3bbcd83050
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-04-16 17:06:33 +0100

    Fix tooltip, add recipe.

--- src/main/java/eutros/fcd/item/ItemDrawersCustomComp.java
+++ src/main/java/eutros/fcd/item/ItemDrawersCustomComp.java
@@ -1,16 +1,25 @@
 package eutros.fcd.item;
 
+import com.jaquadro.minecraft.storagedrawers.StorageDrawers;
 import com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityDrawers;
 import com.jaquadro.minecraft.storagedrawers.item.ItemCustomDrawers;
+import com.mojang.realmsclient.gui.ChatFormatting;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemDrawersCustomComp extends ItemCustomDrawers {
 
@@ -39,6 +48,23 @@ public class ItemDrawersCustomComp extends ItemCustomDrawers {
         }
 
         return true;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation (@Nonnull ItemStack itemStack, @Nullable World world, List<String> list, ITooltipFlag advanced) {
+        NBTTagCompound cmp = itemStack.getTagCompound();
+        boolean hasCompound = itemStack.hasTagCompound() && cmp != null;
+
+        if (hasCompound && cmp.hasKey("material")) {
+            String key = cmp.getString("material");
+            list.add(I18n.format("storagedrawers.material", I18n.format("storagedrawers.material." + key)));
+        }
+
+        list.add(I18n.format("storagedrawers.drawers.description", StorageDrawers.config.getBlockBaseStorage("compdrawers")));
+
+        if (hasCompound && cmp.hasKey("tile"))
+            list.add(ChatFormatting.YELLOW + I18n.format("storagedrawers.drawers.sealed"));
     }
 
 }
