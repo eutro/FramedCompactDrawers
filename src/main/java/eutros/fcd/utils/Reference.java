86cf560ee46f52a84428e5fbe085053a7ff3a63a
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-04-17 03:19:20 +0100

    Who needs automation when you have manual labor?

--- src/main/java/eutros/fcd/utils/Reference.java
+++ src/main/java/eutros/fcd/utils/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
     public static final String MOD_ID = "framedcompactdrawers";
     public static final String NAME = "Framed Compact Drawers";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
 
 }
