6ef46014697e9024d79f53f1d5a2d6586153a30e
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-08-16 13:14:12 +0100

    Disable right click on unframed drawers.

--- src/main/java/eutros/framedcompactdrawers/block/BlockCompDrawersCustom.java
+++ src/main/java/eutros/framedcompactdrawers/block/BlockCompDrawersCustom.java
@@ -1,20 +1,39 @@
 package eutros.framedcompactdrawers.block;
 
 import com.jaquadro.minecraft.storagedrawers.block.BlockCompDrawers;
+import com.jaquadro.minecraft.storagedrawers.block.EnumCompDrawer;
 import com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityDrawers;
+import eutros.framedcompactdrawers.block.tile.IFramingHolder;
 import eutros.framedcompactdrawers.block.tile.TileCompDrawersCustom;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 public class BlockCompDrawersCustom extends BlockCompDrawers {
 
     public BlockCompDrawersCustom(Properties properties) {
         super(properties);
+        setDefaultState(getDefaultState().with(SLOTS, EnumCompDrawer.OPEN3));
     }
 
     @Override
     public TileEntityDrawers createTileEntity(BlockState state, IBlockReader world) {
         return new TileCompDrawersCustom.Slot3();
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
+        TileEntity tile = world.getTileEntity(pos);
+        if(tile instanceof IFramingHolder && !((IFramingHolder) tile).getSide().isEmpty()) {
+            return super.onBlockActivated(state, world, pos, player, hand, hit);
+        }
+        return ActionResultType.PASS;
     }
 
 }
